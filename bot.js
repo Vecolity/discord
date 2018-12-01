f03570d1ce5851e1c9850df332e2eade7263ed42
Vecolity <42125737+Vecolity@users.noreply.github.com>
2018-12-01 14:16:28 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,15 @@
-const commando = require('discord.js-commando');
-const client = new commando.Client();
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
-bot.regristry.registerGroup('random', 'Random');
-bot.regristry.registerDefaults();
-bot.regristry.registerCommandsIn(__dirname = "/commands")
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);

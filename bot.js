650928df6650b3814aad0f33ba0ff45303bc1770
Vecolity <42125737+Vecolity@users.noreply.github.com>
2018-12-01 15:06:17 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,8 @@ const client = new Discord.Client();
 const prefix = 'r!'
 
 client.on('ready', () => {
-    console.log('I am ready!');
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setPresence({ game: { name: 'in like crazy devolopment', type: 'STREAMING' }, status: 'do not disturb' });
 });
 
 client.on('message', message => {

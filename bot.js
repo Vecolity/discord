d93e647070c3712454f45cd3671fc8b56ac413c5
Vecolity <42125737+Vecolity@users.noreply.github.com>
2018-12-01 14:21:55 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,5 +11,12 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'pi') {
+    	message.reply('pong');
+  	}
+});
+
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);

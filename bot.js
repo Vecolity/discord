940651162d96c98e54c010d797bdee038d90a9db
Vecolity <42125737+Vecolity@users.noreply.github.com>
2018-12-01 21:33:43 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,9 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setStatus('dnd');
-    client.user.setActivity('holy ba moly is this in devolopment');
+    console.log('I am ready!');
 });
 
 client.on('message', message => {
@@ -12,13 +10,6 @@ client.on('message', message => {
     	message.reply('pong');
   	}
 });
-
-client.on('message', message => {
-    if (message.content === 'friendsondiscord') {
-    	message.reply('tuan, tito, mary, peter, ben, jose, maxwell, dylan, mohammed and evan');
-  	}
-});
-
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
